7b1ea57a5fcc07aff2dda62e1da6d9e1e8590b63
Sara <sarahsahar0812@gmail.com>
2025-11-28 00:40:37 -0800

    Update power_calculator.py

--- power_calculator.py
+++ power_calculator.py
@@ -1,10 +1,10 @@
-num = input("Enter two-digit number: ")
-
-a = int(num[0])
-b = int(num[1])
-
-result1 = a**b
-result2 = b**a
-
-print(f"{a}^{b} = {result1}")
-print(f"{b}^{a} = {result2}")
+num = input("Enter a two-digit number: ")
+if len(num) == 2 and num.isdigit():
+    a = int(num[0])
+    b = int(num[1])
+    result1 = a**b
+    result2 = b**a
+    print(f"{a}^{b} = {result1}")
+    print(f"{b}^{a} = {result2}")
+else:
+    print("Error: Enter a valid two-digit positive number.")
