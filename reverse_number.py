0dc71e2fc603e3b1460180fc685b2736dd65b0ca
SarahSahar <sarahsahar0812@gmail.com>
2025-11-28 16:56:25 -0800

    Update file

--- reverse_number.py
+++ reverse_number.py
@@ -1,3 +1,13 @@
-n = input("Enter number: ")
-r = "".join(reversed(n))
-print("Reversed:", r)
+num = int(input("Enter a two-digit number: "))
+
+if 10 <= abs(num) <= 99:
+    d1 = abs(num) // 10
+    d2 = abs(num) % 10
+    rev = d2 * 10 + d1
+
+    if num < 0:
+        rev = -rev
+
+    print("Reversed number:", rev)
+else:
+    print("Invalid input!")
